280975905e40e381c3d8c254241523e859ed918d
Edis <edisfejzoski@gmail.com>
2023-11-30 13:58:17 +0100

    finished english translations.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, TranslocoModule],
 })
 export class DashboardModule {}
--- src/app/join-event/join-event.module.ts
+++ src/app/join-event/join-event.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { JoinEventComponent } from './join-event.component';
 import { JoinEventRoutingModule } from './join-event-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [JoinEventComponent],
   imports: [
     CommonModule,
     JoinEventRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslocoModule
   ]
 })
 export class JoinEventModule { }
